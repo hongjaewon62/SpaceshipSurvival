2eea9a6cd27ec53013efce737bd081842fdd3b60
hongjaewon62 <hongjaewon0602@gmail.com>
2024-01-19 23:41:27 +0900

    사운드 슬라이더 저장되도록 수정

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
     public Sound[] bgmSounds, sfxSounds;
     public AudioSource bgmSource, sfxSource;
+    public Slider bgmSlider;
+    public Slider sfxSlider;
 
     private void Awake()
     {
@@ -16,6 +19,25 @@ public class SoundManager : MonoBehaviour
 
     private void Start()
     {
+        if(!PlayerPrefs.HasKey("bgmVolume"))
+        {
+            PlayerPrefs.SetFloat("bgmVolume", 1);
+        }
+        else
+        {
+            LoadBgmVolume();
+        }
+
+        if (!PlayerPrefs.HasKey("sfxVolume"))
+        {
+            PlayerPrefs.SetFloat("sfxVolume", 1);
+        }
+        else
+        {
+            LoadSfxVolume();
+        }
+
+
         PlayBgm("IntroBgm");
     }
 
@@ -51,10 +73,32 @@ public class SoundManager : MonoBehaviour
     public void BgmVolume(float volume)
     {
         bgmSource.volume = volume;
+        SaveBgmVolume();
     }
 
     public void SfxVolume(float volume)
     {
         sfxSource.volume = volume;
+        SaveSfxVolume();
+    }
+
+    private void SaveBgmVolume()
+    {
+        PlayerPrefs.SetFloat("bgmVolume", bgmSlider.value);
+    }
+
+    private void SaveSfxVolume()
+    {
+        PlayerPrefs.SetFloat("sfxVolume", sfxSlider.value);
+    }
+
+    private void LoadBgmVolume()
+    {
+        bgmSlider.value = PlayerPrefs.GetFloat("bgmVolume");
+    }
+
+    private void LoadSfxVolume()
+    {
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
     }
 }
